e3694f82bb61ae628baf8f5476a97c3f11381d9e
hwang <hwang@DESKTOP-C4F8KS1>
2026-05-30 18:06:37 -0700

    update

--- unionFind/839.py
+++ unionFind/839.py
@@ -1,13 +1,15 @@
 class Solution:
     def numSimilarGroups(self, strs: List[str]) -> int:
         n = len(strs)
-        parent = [[i] for i in range(n)]
-        size = [[1] for _ in range(n)]
+        parent = [i for i in range(n)]
+        size = [1 for _ in range(n)]
+        count = n
         def find(x):
             if x != parent[x]:
                 parent[x] = find(parent[x])
             return parent[x]
         def union(a,b):
+            nonlocal count
             root_a = find(a)
             root_b = find(b)
             if root_a != root_b:
@@ -15,4 +17,26 @@ class Solution:
                     root_a , root_b = root_b, root_a
                 parent[root_b] = root_a
                 size[root_a] += size[root_b]
-            
+                count -= 1
+        def is_similar(stra,strb):
+            diff_count = 0
+            for i in range(len(stra)):
+                cha = stra[i]
+                chb = strb[i]
+                if cha != chb:
+                    diff_count += 1
+                if diff_count > 2:
+                    return False
+            return True
+        for i in range(n):
+            for j in range(i+1,n):
+                if is_similar(strs[i],strs[j]):
+                    union(i,j)
+        return count
+    
+# 时间复杂度：
+# O(n^2 * k)
+# 其中 n = len(strs)，k = len(strs[0])。
+# 空间复杂度：
+# O(n)
+# 因为 parent 和 size 都是长度 n。
