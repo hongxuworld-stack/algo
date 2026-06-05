abda0728898bd93319b31de88e9305fbf01af775
hongxu <hongxuworld@gmail.com>
2026-06-05 15:47:16 -0700

    update

--- Graph/treeTraversal/113.py
+++ Graph/treeTraversal/113.py
@@ -4,25 +4,45 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# all records
+# class Solution:
+#     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+#         def dfs(node):
+#             if not node:
+#                 return []
+#             if not node.left and not node.right:
+#                 return [[node.val]]
+#             left_node_list = dfs(node.left)
+#             right_node_list = dfs(node.right)
+#             node_list = []
+#             for lst in left_node_list:
+#                 node_list.append([node.val] + lst)
+#             for lst in right_node_list:
+#                 node_list.append([node.val] + lst)
+#             return node_list
+
+#         root_node_list = dfs(root)
+#         res = []
+#         for lst in root_node_list:
+#             if sum(lst) == targetSum:
+#                 res.append(lst)
+#         return res
+
+#dfs travel
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        def dfs(node):
-            if not node:
-                return []
-            if not node.left and not node.right:
-                return [[node.val]]
-            left_node_list = dfs(node.left)
-            right_node_list = dfs(node.right)
-            node_list = []
-            for lst in left_node_list:
-                node_list.append([node.val] + lst)
-            for lst in right_node_list:
-                node_list.append([node.val] + lst)
-            return node_list
-
-        root_node_list = dfs(root)
         res = []
-        for lst in root_node_list:
-            if sum(lst) == targetSum:
-                res.append(lst)
+        def dfs(node,path,cur_sum):
+            if not node:
+                return
+            path.append(node.val)
+            cur_sum += node.val
+            if not node.left and not node.right:
+                if cur_sum == targetSum:
+                    res.append(path[:])
+            dfs(node.left,path,cur_sum)
+            dfs(node.right,path,cur_sum)
+            path.pop()
+        dfs(root,[],0)
         return res
--- Graph/treeTraversal/530.py
+++ Graph/treeTraversal/530.py
@@ -1,19 +1,33 @@
 class Solution:
-    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        prev = None
-        res = float("inf")
+    # def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+    #     prev = None
+    #     res = float("inf")
 
-        def inorder(node):
-            nonlocal prev, res
+    #     def inorder(node):
+    #         nonlocal prev, res
+    #         if not node:
+    #             return
+    #         # left
+    #         inorder(node.left)
+    #         # root
+    #         if prev is not None:
+    #             res = min(res, node.val - prev)
+    #         prev = node.val
+    #         # right
+    #         inorder(node.right)
+    #     inorder(root)
+    #     return res
+
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        res_list = []
+        res = float("inf")
+        def inOrder(node):
             if not node:
                 return
-            # left
-            inorder(node.left)
-            # root
-            if prev is not None:
-                res = min(res, node.val - prev)
-            prev = node.val
-            # right
-            inorder(node.right)
-        inorder(root)
+            inOrder(node.left)
+            res_list.append(node.val)
+            inOrder(node.right)
+        inOrder(root)
+        for i in range(1,len(res_list)):
+            res = min(res,res_list[i] - res_list[i-1])
         return res
